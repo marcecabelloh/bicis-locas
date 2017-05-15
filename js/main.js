5603bd7b1b4347158921fb6717f0db107e52acd0
marcecabelloherrera <marcecabello.h@gmail.com>
2017-05-15 12:49:59 -0400

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	
+	var nombre = document.getElementById("nombre").value;
+	var apellido = document.getElementById("lastname").value;
+	var email = document.getElementById("input-email").value;
+	var password = document.getElementById("input-password").value;
+	var select = document.getElementsByClassName("form-control").selectedIndex;
+	var twitter = document.getElementById("input-social").value;
+
+
 }
